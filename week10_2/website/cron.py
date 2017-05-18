d4fa4e4751a8114946f474fe3d01d0cdfa040f20
NikolaPavlov <nikola.pavlov1955@gmail.com>
2017-05-18 15:44:28 +0300

    remove the shitty cron

--- week10_2/website/cron.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from .models import Statistics
-
-
-def delete_yesterday_statistic():
-    Statistics.objects.filter(created_at__lt=datetime.now()).delete()
-
-def run():
-    delete_yesterday_statistic()
